59cff00a391e3e0fe4d19ccf4669f76e76a1f65b
Max-IT-ZT <babuskomax@gmail.com>
2024-04-09 17:00:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-$(document).ready(function () {
-  $(".gallery").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false, // Вимикаємо автоматичне прокручування
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
-});
+// $(document).ready(function () {
+//   $(".gallery").slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: false, // Вимикаємо автоматичне прокручування
+//     arrows: true,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 2,
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           slidesToShow: 1,
+//         },
+//       },
+//     ],
+//   });
+// });
